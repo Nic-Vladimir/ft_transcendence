92fa96cfaeea558138d4582d27b67bc0708992a0
styletom <chvataltom@gmail.com>
2026-05-17 18:37:01 +0200

    connect frontend to websocket

---
+++ nextApp/src/hooks/useWebSocket.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useWebSocket(url: string) {
+  const [ws, setWs] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const socket = new WebSocket(url);
+
+    socket.onopen = () => console.log("Connected");
+    socket.onclose = () => console.log("Disconnected");
+
+    setWs(socket);
+
+    return () => socket.close();
+  }, [url]);
+
+  return ws;
+}
