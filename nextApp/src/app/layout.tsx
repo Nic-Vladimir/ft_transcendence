c8106a3f2a118e81d08d00b80fb8117ef4c4e187
styletom <chvataltom@gmail.com>
2026-05-31 22:14:43 +0200

    delete websocket hook/provider

--- nextApp/src/app/layout.tsx
+++ nextApp/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// import { WSProvider } from "@/context/WSContext";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <WSProvider> */}
-          {children}
-        {/* </WSProvider> */}
+        {children}
       </body>
     </html>
   );
